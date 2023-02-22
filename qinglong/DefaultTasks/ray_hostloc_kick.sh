ccde4c800553acd6729ff1b76bf13ad1da8e3d30
Ray <zai7lou@outlook.com>
2023-02-23 00:44:30 +0800

    update

--- qinglong/DefaultTasks/ray_hostloc_kick.sh
+++ qinglong/DefaultTasks/ray_hostloc_kick.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # new Env("hostloc踢楼")
-# cron 5 12,23 * * * ray_hostloc_reply.sh
+# cron 0 * * * * ray_hostloc_kick.sh
 
 . ray_hostloc_base.sh
 
 cd ./src/Ray.Quartz.Hostloc
 # export DOTNET_ENVIRONMENT=Production && \
-export Ray_Hostloc_Run=reply && \
+export Ray_Hostloc_Run=kick && \
 dotnet run --ENVIRONMENT=Production
